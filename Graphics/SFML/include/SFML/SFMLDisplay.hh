80e026cbaee3a05836f573f4a2a7639ee039e9d2
SamuelFlorentin <samuel.florentin@epitech.eu>
2023-03-17 12:17:17 +0100

    [ADD] beginning of the SFMLDisplay implementatino

---
+++ Graphics/SFML/include/SFML/SFMLDisplay.hh
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** src
+** File description:
+** SFMLDisplay
+*/
+
+#pragma once
+
+#include <arcade/interfaces/ADisplay.hh>
+
+class SFMLDisplay : public arc::IDisplay
+{
+};
