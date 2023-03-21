61fd9a1727fea55df38fe3f4ce03d4f2da1b4184
LouisB <louis.bassagal@epitech.eu>
2023-03-21 09:24:12 +0100

    [MODIFY] Split IEntity by two, ISprite & IText + Delete all things created with IEntity

---
+++ Core/include/arcade/interfaces/ISprite.hh
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2023
+** Arcade
+** File description:
+** ISprite
+*/
+
+#pragma once
+
+#include <string>
+#include <arcade/Geometry.hh>
+#include <arcade/interfaces/IWindow.hh>
+
+namespace arc {
+
+    class ISprite {
+        public:
+            ISprite() = default;
+            ISprite(ISprite &&) = delete;
+            ISprite(const ISprite &) = default;
+            virtual ~ISprite() = 0;
+
+            ISprite &operator=(const ISprite&) = default;
+            ISprite &operator=(ISprite &&) = delete;
+
+            virtual void createSprite(std::string str) = 0;
+            virtual void createSprite(ISprite &) = 0;
+            [[nodiscard]] virtual ISprite &getSprite() = 0;
+            virtual void destroySprite() = 0;
+            virtual void moveSprite(arc::Vector vector) = 0;
+            virtual void setPosition(arc::Vector vector) = 0;
+            virtual void setSize(arc::Vector vector) = 0;
+            virtual void setRectangle(arc::Rect rectangle) = 0;
+            virtual void drawSprite(IWindow &window) = 0;
+    };
+}
