4fbcf05ecf2a3992c8573b1620315c3ab3e438a5
SamuelFlorentin <samuel.florentin@epitech.eu>
2023-03-21 12:02:57 +0100

    [ADD] SFML display implementation

--- Graphics/SFML/include/SFML/SFMLDisplay.hh
+++ Graphics/SFML/include/SFML/SFMLDisplay.hh
@@ -5,10 +5,29 @@
 ** SFMLDisplay
 */
 
-#pragma once
-
+#include <SFML/Window/Event.hpp>
 #include <arcade/interfaces/ADisplay.hh>
+#include <arcade/interfaces/IWindow.hh>
 
-class SFMLDisplay : public arc::IDisplay
+struct EventLink {
+        arc::Event s_event;
+        sf::Keyboard::Key s_key_code;
+};
+
+class SFMLDisplay : public arc::ADisplay
 {
+    public:
+        arc::Event GetEvent() final;
+        SFMLDisplay();
+
+        SFMLDisplay(SFMLDisplay const &to_copy) = delete;
+        SFMLDisplay(SFMLDisplay &&to_move) = default;
+        ~SFMLDisplay() override = default;
+        SFMLDisplay &operator=(SFMLDisplay const &to_copy) = delete;
+        SFMLDisplay &operator=(SFMLDisplay &&to_move) = default;
+
+    private:
+        arc::Event analyse_key_pressed();
+        sf::Event m_event;
+        std::vector<EventLink> m_event_list;
 };
