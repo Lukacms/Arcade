6c803095b3514434165f3d3811ef7c5f1ba77b32
SamuelFlorentin <samuel.florentin@epitech.eu>
2023-03-22 13:52:58 +0100

    [FIX] Factory because we wasnt able to set path

--- Core/include/arcade/interfaces/IFactory.hh
+++ Core/include/arcade/interfaces/IFactory.hh
@@ -22,7 +22,7 @@ namespace arc
             IFactory &operator=(IFactory const &to_copy) = default;
             IFactory &operator=(IFactory &&to_move) = delete;
             virtual ~IFactory();
-            virtual std::unique_ptr<ISprite> CreateISprite() = 0;
-            virtual std::unique_ptr<IText> CreateIText() = 0;
+            virtual std::unique_ptr<ISprite> CreateISprite(const std::string &path) = 0;
+            virtual std::unique_ptr<IText> CreateIText(const std::string &path) = 0;
     };
 } // namespace arc
