d770a29604a1d739f4eeeb52c63ef065673e23a4
SamuelFlorentin <samuel.florentin@epitech.eu>
2023-03-22 12:33:15 +0100

    [FIX] Factory

--- Core/include/arcade/interfaces/IFactory.hh
+++ Core/include/arcade/interfaces/IFactory.hh
@@ -7,7 +7,8 @@
 
 #pragma once
 
-#include <arcade/interfaces/IEntity.hh>
+#include <arcade/interfaces/ISprite.hh>
+#include <arcade/interfaces/IText.hh>
 #include <memory>
 
 namespace arc
@@ -15,8 +16,13 @@ namespace arc
     class IFactory
     {
         public:
+            IFactory() = default;
+            IFactory(IFactory const &to_copy) = default;
+            IFactory(IFactory &&to_move) = delete;
+            IFactory &operator=(IFactory const &to_copy) = default;
+            IFactory &operator=(IFactory &&to_move) = delete;
             virtual ~IFactory();
-            virtual std::unique_ptr<IEntity> CreateIEntity(const std::string &type) = 0;
-            virtual std::unique_ptr<IEntity> ResetEntity(const std::string &type) = 0;
+            virtual std::unique_ptr<ISprite> CreateISprite() = 0;
+            virtual std::unique_ptr<IText> CreateIText() = 0;
     };
 } // namespace arc
