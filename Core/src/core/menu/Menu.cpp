a5b41eb3bcf56c74d47781e94834e55bcafd67d9
Luka Camus <luka.camus@epitech.eu>
2023-03-28 13:57:06 +0200

    [ADD] Menu => placeholder display menu function to compile
    need to implement it

---
+++ Core/src/core/menu/Menu.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** menu
+** File description:
+** Menu
+*/
+
+#include <arcade/core/Menu.hh>
+
+void arc::Menu::display(IWindow &window) const
+{
+    window.UpdateWindow();
+}
