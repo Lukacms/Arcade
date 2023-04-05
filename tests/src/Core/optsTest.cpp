274a69e38f2a40fb8f122503bfff5deee68b203e
Luka Camus <luka.camus@epitech.eu>
2023-04-05 15:36:46 +0200

    [ADD][UNIT.TEST] added some more tests
    can't really add much more, as we can't test the greaphical element

--- tests/src/Core/optsTest.cpp
+++ tests/src/Core/optsTest.cpp
@@ -9,8 +9,9 @@
 #include <arcade/Opts.hh>
 #include <catch2/catch.hpp>
 #include <string>
+#include <sys/types.h>
 
-TEST_CASE("Basic testing for Opts", "oui")
+TEST_CASE("Basic testing for Opts", "directory")
 {
     arc::Opts opts{"."};
 
@@ -21,7 +22,20 @@ TEST_CASE("Basic testing for Opts", "oui")
     }
 }
 
-TEST_CASE("testing with good library", "oui")
+TEST_CASE("Basic testing for Opts with an invalid file")
+{
+    arc::Opts opts{"./ieloqhd"};
+
+    try {
+        opts.getOpts();
+    } catch (arc::Opts::OptsException &e) {
+        REQUIRE(
+            e.what() ==
+            std::string{"./ieloqhd: cannot open shared object file: No such file or directory"});
+    }
+}
+
+TEST_CASE("testing with good library", "loader")
 {
     arc::Opts opts{"./lib/arcade_ncurses.so"};
     arc::Core core;
@@ -32,4 +46,18 @@ TEST_CASE("testing with good library", "oui")
         REQUIRE(e.what() == LIB_FORMAT_ERR);
     }
     REQUIRE(core.getSharedDisplays() == DISPLAYS);
+    REQUIRE(core.getSharedGames() == GAMES);
+}
+
+TEST_CASE("basic things")
+{
+    arc::Opts opts{"./lib/arcade_sdl2.so"};
+    arc::Core core;
+
+    try {
+        core = opts.getOpts();
+    } catch (arc::Opts::OptsException &e) {
+        REQUIRE(1 == 2);
+    }
+    REQUIRE(core.getMode() == arc::CoreMode::Menu);
 }
